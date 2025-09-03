27fd798db50faf8cdca3aa2c0f5bce892f963af4
R4falek <rafal888@onet.pl>
2025-09-03 11:48:45 +0200

    feat: add ThemeToggler

--- app/components/ThemeToggler/ThemeToggler.Component.tsx
+++ app/components/ThemeToggler/ThemeToggler.Component.tsx
@@ -2,22 +2,36 @@
 
 import { useThemeStore } from '@/app/stores/themeStore'
 import { useEffect } from 'react'
+import { Moon, Sun } from 'lucide-react';
+import styles from './ThemeToggle.module.scss'
 
 export const ThemeToggle = () => {
   const theme = useThemeStore(state => state.theme)
   const toggleTheme = useThemeStore(state => state.toggleTheme)
   const setTheme = useThemeStore(state => state.setTheme)
 
-
   useEffect(() => {
     if (!theme) {return}
     setTheme(theme)
-    console.log('object');
   }, [setTheme, theme])
 
   return (
-    <button onClick={toggleTheme}>
-      {theme === 'light' ? '🌙' : '☀️'} {theme}
+    <button
+      onClick={toggleTheme}
+      className={styles.themeToggle}
+      aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
+      title={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
+    >
+      <div className={styles.iconContainer}>
+        <Sun
+          className={`${styles.icon} ${styles.sunIcon} ${theme === 'light' ? styles.active : ''}`}
+          size={24}
+        />
+        <Moon
+          className={`${styles.icon} ${styles.moonIcon} ${theme === 'dark' ? styles.active : ''}`}
+          size={24}
+        />
+      </div>
     </button>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Button from './components/Button/Button.Component';
 import { ThemeToggle } from './components/ThemeToggler/ThemeToggler.Component';
 
 export default function Home() {
   return (
     <div>
       <ThemeToggle />
-      <Button />
-      elo
     </div>
   );
 }
